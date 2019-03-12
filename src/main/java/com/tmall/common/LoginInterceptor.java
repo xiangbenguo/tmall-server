ba3c6d31ce680fce6373e4c9915d16ae637e93ee
yxisme <yxisme@foxmail.com>
2019-03-12 10:29:57 +0800

    关闭全局登录验证功能，便于测试

--- src/main/java/com/tmall/common/LoginInterceptor.java
+++ src/main/java/com/tmall/common/LoginInterceptor.java
@@ -12,10 +12,10 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        User user = (User) request.getSession().getAttribute(GlobalHandler.SESSION_USER);
-        if (user == null || user.getId() == 0) {
-            throw new MyException(CodeMessageDef.USER_NOT_LOGGED_IN);
-        }
+//        User user = (User) request.getSession().getAttribute(GlobalHandler.SESSION_USER);
+//        if (user == null || user.getId() == 0) {
+//            throw new MyException(CodeMessageDef.USER_NOT_LOGGED_IN);
+//        }
         return true;
     }
 }
